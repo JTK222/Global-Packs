11e897b1c4600031a167b2646b84fa050d59a67d
JTK222 | Lukas <jtk222.lukas@gmail.com>
2019-08-04 18:47:23 +0200

    1.14.4 port, also decreased file size out of fun (needs more features)

--- src/main/java/net/dark_roleplay/drpglobaldatapack/DarkRoleplayGlobalDatapack.java
+++ src/main/java/net/dark_roleplay/drpglobaldatapack/DarkRoleplayGlobalDatapack.java
@@ -1,42 +1,36 @@
 package net.dark_roleplay.drpglobaldatapack;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.Writer;
+import java.util.Map;
 
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.resources.FilePack;
+import net.minecraft.resources.FolderPack;
+import net.minecraft.resources.IPackFinder;
+import net.minecraft.resources.ResourcePackInfo;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 
-@Mod(DarkRoleplayGlobalDatapack.MODID)
-public class DarkRoleplayGlobalDatapack {
-	public static final String MODID = "drpglobaldatapack";
-	
-	public static Object INSTANCE;
-	
-	private static final String DEFAULT_PACK_META = "{\r\n" + 
-			"	\"pack\":{\r\n" + 
-			"		\"pack_format\":1,\r\n" + 
-			"		\"description\":\"Global Datapack - using Dark Roleplays, Global Datapack mod\"\r\n" + 
-			"	}\r\n" + 
-			"}";
-	
-	public DarkRoleplayGlobalDatapack() {
-		DarkRoleplayGlobalDatapack.INSTANCE = this;
-        MinecraftForge.EVENT_BUS.register(new ServerLaunchListener());
-        
-		File globalDatapack = new File("./global_data_pack/");
-		globalDatapack.mkdirs();
-		
-		File packMeta = new File(globalDatapack, "pack.mcmeta");
-		if(!packMeta.exists()) {
-			try {
-				packMeta.createNewFile();
-				try(Writer writer = new FileWriter(packMeta)){
-					writer.write(DEFAULT_PACK_META);
+@Mod("globaldatapack")
+@Mod.EventBusSubscriber
+public final class DarkRoleplayGlobalDatapack {
+
+	@SubscribeEvent
+	public void serverStarting(FMLServerAboutToStartEvent event) {
+		event.getServer().getResourcePacks().addPackFinder(new IPackFinder() {
+			@Override
+			public <T extends ResourcePackInfo> void addPackInfosToMap(Map<String, T> nameToPackMap, ResourcePackInfo.IFactory<T> packInfoFactory) {
+				File globalDatapacks = new File("./global_data_packs/");
+				globalDatapacks.mkdirs();
+				if(globalDatapacks.exists() && globalDatapacks.isDirectory()) {
+					for (File file : globalDatapacks.listFiles()) {
+						if (!file.isDirectory() && !file.getName().endsWith(".zip")) continue;
+						T t = ResourcePackInfo.createResourcePack("modpack:" + file.getName(), false, () -> file.isDirectory() ? new FolderPack(file) : new FilePack(file), packInfoFactory, ResourcePackInfo.Priority.BOTTOM);
+						if (t == null) continue;
+						nameToPackMap.put("modpack:" + file.getName(), t);
+					}
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
-		}
+		});
 	}
 }
