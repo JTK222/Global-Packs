e410adc6af5bab59e16fbf4f2bfd5795095ffbd1
JTK222 <jtk222.lukas@gmail.com>
2020-06-27 23:42:24 +0200

    1.16 Port

--- src/main/java/net/dark_roleplay/drpglobaldatapack/DarkRoleplayGlobalDatapack.java
+++ src/main/java/net/dark_roleplay/drpglobaldatapack/DarkRoleplayGlobalDatapack.java
@@ -1,15 +1,16 @@
 package net.dark_roleplay.drpglobaldatapack;
 
-import java.io.File;
-import java.util.Map;
-
 import net.minecraft.resources.FilePack;
 import net.minecraft.resources.FolderPack;
 import net.minecraft.resources.IPackFinder;
 import net.minecraft.resources.ResourcePackInfo;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
+
+import java.io.File;
+import java.util.function.Consumer;
 
 @Mod("globaldatapack")
 @Mod.EventBusSubscriber(modid="globaldatapack")
@@ -20,16 +21,16 @@ public final class DarkRoleplayGlobalDatapack {
 
 		event.getServer().getResourcePacks().addPackFinder(new IPackFinder() {
 			@Override
-			public <T extends ResourcePackInfo> void addPackInfosToMap(Map<String, T> nameToPackMap, ResourcePackInfo.IFactory<T> packInfoFactory) {
+			public <T extends ResourcePackInfo> void func_230230_a_(Consumer<T> consumer, ResourcePackInfo.IFactory<T> iFactory) {
 				File globalDatapacks = event.getServer().getFile("global_data_pack");
 
 				globalDatapacks.mkdirs();
 				if(globalDatapacks.exists() && globalDatapacks.isDirectory()) {
 					for (File file : globalDatapacks.listFiles()) {
 						if (!file.isDirectory() && !file.getName().endsWith(".zip")) continue;
-						T t = ResourcePackInfo.createResourcePack("modpack:" + file.getName(), false, () -> file.isDirectory() ? new FolderPack(file) : new FilePack(file), packInfoFactory, ResourcePackInfo.Priority.TOP);
+						T t = ResourcePackInfo.createResourcePack(file.getName(), false, () -> file.isDirectory() ? new FolderPack(file) : new FilePack(file), iFactory, ResourcePackInfo.Priority.TOP, text -> new TranslationTextComponent("global_datapack.pack", text));
 						if (t == null) continue;
-						nameToPackMap.put("modpack:" + file.getName(), t);
+						consumer.accept(t);
 					}
 				}
 			}
