504bd167d99d9ee14352600f11bf9ad4ddb1c531
JTK222 | Lukas <jtk222.lukas@gmail.com>
2022-06-08 00:04:43 +0200

    1.19 Port

--- Common/src/main/java/net/dark_roleplay/gdarp/mixin/ClientPackFinderMixin.java
+++ Common/src/main/java/net/dark_roleplay/gdarp/mixin/ClientPackFinderMixin.java
@@ -24,13 +24,4 @@ public class ClientPackFinderMixin {
 	private RepositorySource[] addClientPackFinder(RepositorySource[] arg) {
 		return ArrayUtils.addAll(arg, CommonClass.getRepositorySource(PackType.CLIENT_RESOURCES, true));
 	}
-
-	@ModifyArg(
-			method = "createPackRepository",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/repository/PackRepository;<init>(Lnet/minecraft/server/packs/PackType;[Lnet/minecraft/server/packs/repository/RepositorySource;)V"),
-			index = 1
-	)
-	private static RepositorySource[] addClientPackFinder2(RepositorySource[] arg) {
-		return ArrayUtils.addAll(arg, CommonClass.getRepositorySource(PackType.SERVER_DATA, true), CommonClass.getRepositorySource(PackType.SERVER_DATA, false));
-	}
 }
--- Fabric/src/main/java/net/dark_roleplay/gdarp/GlobalDataAndResourcepacks.java
+++
@@ -1,8 +0,0 @@
-package net.dark_roleplay.gdarp;
-
-import net.fabricmc.api.ModInitializer;
-
-public class GlobalDataAndResourcepacks implements ModInitializer {
-	@Override
-	public void onInitialize() {}
-}
