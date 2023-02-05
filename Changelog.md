dceb3675d275e218e2bb59e1907a0ffb21b188d7
JTK222 <jtk222.lukas@gmail.com>
2023-02-05 17:47:48 +0100

    Bugfixes and gradle script improvements

--- Changelog.md
+++ Changelog.md
@@ -1,2 +1,11 @@
+### Global Packs 1.15.3 for Minecraft 1.19.3
+(Fix) Fixed optional datapacks, not being optional.
+
+### Global Packs 1.15.2 for Minecraft 1.19.3
+(Fix) Resourcepacks being treated as datapacks, for datapack loading out of resourcepacks folder
+
+### Global Packs 1.15.1 for Minecraft 1.19.3 (Forge Only)
+(Fix) Fixed Crash caused by incorrect mixin file being expected
+
 ### Global Packs 1.15 for Minecraft 1.19.3
-\* Full rewrite of the mod all known previous issues are gone, if you find new ones, please report them.  
+(Fix) Full rewrite of the mod all known previous issues are gone, if you find new ones, please report them.  
--- Common/src/main/java/net/dark_roleplay/gdarp/pack_finders/GlobalPackFinder.java
+++ Common/src/main/java/net/dark_roleplay/gdarp/pack_finders/GlobalPackFinder.java
@@ -17,13 +17,47 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Set;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class GlobalPackFinder implements RepositorySource {
 
+	private static final Predicate<Path> IS_VALID_RESOURCE_PACK = (pack) -> {
+		boolean flag = true;
+		if (Files.isRegularFile(pack) && pack.toString().endsWith(".zip")) {
+			try (FileSystem fs = FileSystems.newFileSystem(pack)){
+				flag &= Files.isDirectory(fs.getPath("assets/"));
+				flag &= Files.isRegularFile(fs.getPath("pack.mcmeta"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			flag &= Files.isDirectory(pack.resolve("assets/"));
+			flag &= Files.isRegularFile(pack.resolve("pack.mcmeta"));
+		}
+		return flag;
+	};
+
+	private static final Predicate<Path> IS_VALID_DATA_PACK = (pack) -> {
+		boolean flag = true;
+		if (Files.isRegularFile(pack) && pack.toString().endsWith(".zip")) {
+			try (FileSystem fs = FileSystems.newFileSystem(pack)){
+				flag &= Files.isDirectory(fs.getPath("data/"));
+				flag &= Files.isRegularFile(fs.getPath("pack.mcmeta"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			flag &= Files.isDirectory(pack.resolve("data"));
+			flag &= Files.isRegularFile(pack.resolve("pack.mcmeta"));
+		}
+		return flag;
+	};
+
 	private static PackSource GLOBAL = PackSource.create(name -> name.copy().append(" (Global)").withStyle(ChatFormatting.AQUA), true);
-	private static PackSource GLOBAL_OPT = PackSource.create(name -> name.copy().append(" (Global Optional)").withStyle(ChatFormatting.DARK_AQUA), true);
+	private static PackSource GLOBAL_OPT = PackSource.create(name -> name.copy().append(" (Global Optional)").withStyle(ChatFormatting.DARK_AQUA), false);
 
 	private final PackType packType;
 	private final boolean forcedPacks;
@@ -40,13 +74,13 @@ public class GlobalPackFinder implements RepositorySource {
 	public void loadPacks(Consumer<Pack> packRegistrar) {
 		discoverResourcePacks(path -> {
 			Pack.ResourcesSupplier resourceSupplier = null;
-			if(Files.isRegularFile(path) && path.toString().endsWith(".zip"))
+			if (Files.isRegularFile(path) && path.toString().endsWith(".zip"))
 				resourceSupplier = createFilePack(path);
 			else resourceSupplier = createFolderPack(path);
 
-			if(resourceSupplier == null) return;
+			if (resourceSupplier == null) return;
 
-			Pack pack =  Pack.readMetaAndCreate(
+			Pack pack = Pack.readMetaAndCreate(
 					path.getFileName().toString(),
 					Component.literal(path.getFileName().toString()),
 					this.forcedPacks, resourceSupplier,
@@ -55,43 +89,54 @@ public class GlobalPackFinder implements RepositorySource {
 					this.forcedPacks ? GLOBAL : GLOBAL_OPT
 			);
 
-			if(pack != null) packRegistrar.accept(pack);
+			if (pack != null) packRegistrar.accept(pack);
 		});
 	}
 
-	private Pack.ResourcesSupplier createFilePack(Path path){
+	private Pack.ResourcesSupplier createFilePack(Path path) {
 		Pack.ResourcesSupplier supplier = null;
 		FileSystem fs = path.getFileSystem();
-		if(fs == FileSystems.getDefault() || fs instanceof LinkFileSystem)
+
+		if (this.packType == PackType.CLIENT_RESOURCES && !IS_VALID_RESOURCE_PACK.test(path)) return null;
+		if (this.packType == PackType.SERVER_DATA && !IS_VALID_DATA_PACK.test(path)) return null;
+
+
+		if (fs == FileSystems.getDefault() || fs instanceof LinkFileSystem)
 			return (needle) -> new FilePackResources(needle, path.toFile(), false);
 
 		return null;
-	}
+}
 
-	private Pack.ResourcesSupplier createFolderPack(Path path){
+	private Pack.ResourcesSupplier createFolderPack(Path path) {
+		if (this.packType == PackType.CLIENT_RESOURCES && !IS_VALID_RESOURCE_PACK.test(path)) return null;
+		if (this.packType == PackType.SERVER_DATA && !IS_VALID_DATA_PACK.test(path)) return null;
+
 		return (needle) -> new PathPackResources(needle, path, false);
 	}
 
-	private void discoverResourcePacks(Consumer<Path> packCallback){
-		for(Path path : this.packLocations){
-			if(Files.isDirectory(path)){
-				if(Files.isRegularFile(path.resolve("pack.mcmeta"))){
+	private void discoverResourcePacks(Consumer<Path> packCallback) {
+		for (Path path : this.packLocations) {
+			if (Files.isDirectory(path)) {
+				if (Files.isRegularFile(path.resolve("pack.mcmeta"))) {
 					packCallback.accept(path);
 					continue;
 				}
 
 				try {
-					if(Files.notExists(path)) Files.createDirectories(path);
+					if (Files.notExists(path)) Files.createDirectories(path);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 
-				try(Stream<Path> fileStream = Files.list(path)) {
-					fileStream.forEach(packCallback);
+				try (Stream<Path> fileStream = Files.list(path)) {
+					fileStream.forEach(filePath -> {
+						if (Files.isRegularFile(filePath) || (Files.isDirectory(filePath) && Files.isRegularFile(filePath.resolve("pack.mcmeta"))))
+							packCallback.accept(filePath);
+					});
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			}else{
+			} else {
 				packCallback.accept(path);
 			}
 		}
